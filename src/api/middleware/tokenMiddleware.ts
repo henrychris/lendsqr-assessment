8b6f582d61ce680206943f8a539e516321784b0c
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-16 17:18:40 +0100

    Create tokenMiddleware.ts

---
+++ src/api/middleware/tokenMiddleware.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+
+export function extractUserIdFromToken(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith("Bearer ")) {
+        return res
+            .status(401)
+            .json({ error: "Token not provided or invalid format." });
+    }
+
+    try {
+        const token = authorization.split(" ")[1];
+        const parts = token.split("-");
+
+        if (parts.length !== 3) {
+            return res.status(400).json({ error: "Invalid token format." });
+        }
+
+        const userId = parts[1];
+        req.body.userId = userId;
+
+        next();
+    } catch (error) {
+        console.error("Error extracting user ID from token:", error);
+        res.status(500).json({ error: "Something went wrong." });
+    }
+}
