87a2d038e503c4350de73bc806e3dd016be10733
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-16 11:07:43 +0100

    setup server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,28 @@
-console.log("server still here!");
+import express, { Request, Response, NextFunction } from "express";
+import logger from "morgan";
+import createError from "http-errors";
+
+const app = express();
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// catch 404 and forward to error handler
+app.use(function (_req: Request, _res: Response, next: NextFunction) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err: any, req: Request, res: Response, _next: NextFunction) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    res.status(err.status || 500);
+    res.send({ message: "error", error: err.status });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
