6b430ebfd9e3e715b761a7faa9206bc344f91f23
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-17 16:44:33 +0100

    add default mock for bcrypt

--- __mocks__/bcrypt.ts
+++ __mocks__/bcrypt.ts
@@ -1,4 +1,4 @@
 import { vi } from "vitest";
 
 export const hash = vi.fn().mockResolvedValue("hashed-password");
-export const compare = vi.fn();
+export const compare = vi.fn().mockResolvedValue(true);
