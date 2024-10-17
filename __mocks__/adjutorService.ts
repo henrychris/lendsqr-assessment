8e64aa7431015fe6234dfe8bf2645271ec253d77
henrychris <64085210+henrychris@users.noreply.github.com>
2024-10-17 16:34:48 +0100

    mock bcrypt and adjutor service

---
+++ __mocks__/adjutorService.ts
@@ -0,0 +1,3 @@
+import { vi } from "vitest";
+
+export const checkBlacklist = vi.fn().mockResolvedValue(false);
---
+++ __mocks__/bcrypt.ts
@@ -0,0 +1,4 @@
+import { vi } from "vitest";
+
+export const hash = vi.fn().mockResolvedValue("hashed-password");
+export const compare = vi.fn();
